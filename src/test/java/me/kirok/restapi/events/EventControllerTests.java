e108a0080a643e9c3d29deabbcd0ab81bc48db5d
psick22 <kgirok@gmail.com>
2021-06-11 17:44:02 +0900

    polishing

--- src/test/java/me/kirok/restapi/events/EventControllerTests.java
+++ src/test/java/me/kirok/restapi/events/EventControllerTests.java
@@ -2,6 +2,7 @@ package me.kirok.restapi.events;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -54,7 +56,9 @@ public class EventControllerTests {
         )
             .andDo(print())
             .andExpect(status().isCreated())
-            .andExpect(jsonPath("id").exists());
+            .andExpect(jsonPath("id").exists())
+            .andExpect(header().exists(HttpHeaders.LOCATION))
+            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
     }
 
 
