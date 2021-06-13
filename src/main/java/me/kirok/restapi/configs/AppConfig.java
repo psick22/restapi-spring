be38d255e1dccfbb55e0196d832ad3eb12e56a15
psick22 <kgirok@gmail.com>
2021-06-13 19:52:04 +0900

    spring security form auth config

--- src/main/java/me/kirok/restapi/configs/AppConfig.java
+++ src/main/java/me/kirok/restapi/configs/AppConfig.java
@@ -1,6 +1,13 @@
 package me.kirok.restapi.configs;
 
+import java.util.Set;
+import me.kirok.restapi.accounts.Account;
+import me.kirok.restapi.accounts.AccountRole;
+import me.kirok.restapi.accounts.AccountService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -18,6 +25,27 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+
+                Account account = Account.builder()
+                    .email("kgirok@gmail.com")
+                    .password("123123")
+                    .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                    .build();
+
+                accountService.saveAccount(account);
+
+            }
+        };
     }
 
 }
--- src/main/java/me/kirok/restapi/configs/SecurityConfig.java
+++ src/main/java/me/kirok/restapi/configs/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -49,10 +50,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests().mvcMatchers("docs/index.html").anonymous();
-//        http.authorizeRequests()
-//            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).anonymous();
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .anonymous().and()
+            .formLogin().and()
+            .authorizeRequests()
+            .mvcMatchers(HttpMethod.GET, "/api/**").authenticated()
+            .anyRequest().authenticated();
+    }
 }
